7b82f1bea1050f021a6d3f8b507fb43438016c55
Gab-gi <gn31323@gmail.com>
2025-02-04 14:19:04 +0000

    criei um printf de desafio de logica

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,43 +1,76 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 2 - Comparação das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de comparação de cartas de cidades. 
-// Siga os comentários para implementar cada parte do desafio.
-
 int main() {
-    // Definição das variáveis para armazenar as propriedades das cidades
-    // Você pode utilizar o código do primeiro desafio
+    // Definição das variáveis
+    int codigo_cidade1, codigo_cidade2;
+    float area1, populacao1, populacao_por_k2_1;
+    float area2, populacao2, populacao_por_k2_2;
 
-    
-    // Cadastro das Cartas:
-    // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
-    // utilizando a função scanf para capturar as entradas.
-    // utilize o código do primeiro desafio
+    // Cadastro da primeira cidade
+    printf("Digite o código da primeira cidade:\n");
+    scanf("%d", &codigo_cidade1);
 
-    // Exemplo:
-    // printf("Digite o código da cidade: ");
-    // scanf("%s", codigo);
-    // 
-    // (Repita para cada propriedade)
+    printf("Digite a área da primeira cidade (km²):\n");
+    scanf("%f", &area1);
 
-    // Comparação de Cartas:
-    // Desenvolva a lógica de comparação entre duas cartas.
-    // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
+    printf("Digite a população da primeira cidade:\n");
+    scanf("%f", &populacao1);
 
-    // Exemplo:
-    // if (populacaoA > populacaoB) {
-    //     printf("Cidade 1 tem maior população.\n");
-    // } else {
-    //     printf("Cidade 2 tem maior população.\n");
-    // }
+    // Cálculo da densidade populacional
+    populacao_por_k2_1 = populacao1 / area1;
 
-    // Exibição dos Resultados:
-    // Após realizar as comparações, exiba os resultados para o usuário.
-    // Certifique-se de que o sistema mostre claramente qual carta venceu e com base em qual atributo.
+    // Cadastro da segunda cidade
+    printf("\nDigite o código da segunda cidade:\n");
+    scanf("%d", &codigo_cidade2);
 
-    // Exemplo:
-    // printf("A cidade vencedora é: %s\n", cidadeVencedora);
+    printf("Digite a área da segunda cidade (km²):\n");
+    scanf("%f", &area2);
+
+    printf("Digite a população da segunda cidade:\n");
+    scanf("%f", &populacao2);
+
+    // Cálculo da densidade populacional
+    populacao_por_k2_2 = populacao2 / area2;
+
+    // Exibição dos dados cadastrados
+    printf("\nCidade 1:\n");
+    printf("Código: %d\n", codigo_cidade1);
+    printf("Área: %.2f km²\n", area1);
+    printf("População: %.2f habitantes\n", populacao1);
+    printf("Densidade populacional: %.2f hab/km²\n", populacao_por_k2_1);
+
+    printf("\nCidade 2:\n");
+    printf("Código: %d\n", codigo_cidade2);
+    printf("Área: %.2f km²\n", area2);
+    printf("População: %.2f habitantes\n", populacao2);
+    printf("Densidade populacional: %.2f hab/km²\n", populacao_por_k2_2);
+
+    // Comparação das cidades e determinação da vencedora
+    printf("\nComparação das cidades:\n");
+
+    if (populacao1 > populacao2) {
+        printf("Cidade 1 tem maior população.\n");
+    } else if (populacao2 > populacao1) {
+        printf("Cidade 2 tem maior população.\n");
+    } else {
+        printf("Ambas as cidades têm a mesma população.\n");
+    }
+
+    if (area1 > area2) {
+        printf("Cidade 1 tem maior área territorial.\n");
+    } else if (area2 > area1) {
+        printf("Cidade 2 tem maior área territorial.\n");
+    } else {
+        printf("Ambas as cidades têm a mesma área.\n");
+    }
+
+    if (populacao_por_k2_1 > populacao_por_k2_2) {
+        printf("Cidade 1 tem maior densidade populacional.\n");
+    } else if (populacao_por_k2_2 > populacao_por_k2_1) {
+        printf("Cidade 2 tem maior densidade populacional.\n");
+    } else {
+        printf("Ambas as cidades têm a mesma densidade populacional.\n");
+    }
 
     return 0;
 }
